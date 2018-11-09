40f94077bc7bff4f1cb5f7720c6cd467c2b4ddf9
MkfsSion <mkfssion@mkfssion.com>
2018-11-09 12:02:47 +0100

    ccache: some misc fixed

--- build_rom.sh
+++ build_rom.sh
@@ -61,8 +61,7 @@ make clobber
 
 # Use ccache for builds #
 export USE_CCACHE=1
-export CCACHE_DIR=$HOME/.ccache
-Prebuilts/misc/linux-x86/ccache/ccache -M 50G
+export CCACHE_DIR=$HOME/.ccache && prebuilts/misc/linux-x86/ccache/ccache -M 50G
 
 lunch $PRODUCTNAME-$BUILDTYPE
 assert_unequal $? 0 "Failed to lunch"
